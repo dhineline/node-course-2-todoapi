7ca21f3a34eaf3a407b230bc9534977faec170bf
Douglas Hineline <douglas.hineline@givesmart.com>
2018-03-12 11:14:53 -0400

    tests for GET todos by id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 const expect = require('expect');
 const request = require('supertest');
 
@@ -5,9 +7,11 @@ const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 },
 {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }]
 
@@ -70,3 +74,32 @@ describe('GET /todos', () => {
       .end(done);
   });
 });
+
+
+describe('Get /todos/:id', () =>{
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done);
+  })
+
+it('should return 404 if todo not found', (done) => {
+  var hexID = new ObjectID().toHexString();
+  request(app)
+    .get(`/todos/${hexID}`)
+    .expect(404)
+    .end(done);
+});
+
+it('should return 404 for non-object ids', (done) => {
+  request(app)
+    .get('/todos/123abc')
+    .expect(404)
+    .end(done);
+});
+
+})
